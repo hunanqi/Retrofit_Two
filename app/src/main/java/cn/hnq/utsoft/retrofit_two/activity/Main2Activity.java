4d6711dd37b857c6493a28081d7e6f2fc13b492c
胡楠启 <nan-qi.hu@utsoft.cn>
2017-03-02 15:14:22 +0800

    测试001

--- app/src/main/java/cn/hnq/utsoft/retrofit_two/activity/Main2Activity.java
+++ app/src/main/java/cn/hnq/utsoft/retrofit_two/activity/Main2Activity.java
@@ -1,6 +1,7 @@
 package cn.hnq.utsoft.retrofit_two.activity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import cn.hnq.utsoft.libary.BaseActivity;
 import cn.hnq.utsoft.retrofit_two.R;
@@ -11,5 +12,6 @@ public class Main2Activity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Log.i("sss","sss");
     }
 }
