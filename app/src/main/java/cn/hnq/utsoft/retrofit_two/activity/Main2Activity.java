f8d2362448fe64c4ef19b2f11fe1c238858356ec
胡楠启 <nan-qi.hu@utsoft.cn>
2017-03-02 15:13:53 +0800

    测试

---
+++ app/src/main/java/cn/hnq/utsoft/retrofit_two/activity/Main2Activity.java
@@ -0,0 +1,15 @@
+package cn.hnq.utsoft.retrofit_two.activity;
+
+import android.os.Bundle;
+
+import cn.hnq.utsoft.libary.BaseActivity;
+import cn.hnq.utsoft.retrofit_two.R;
+
+public class Main2Activity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
